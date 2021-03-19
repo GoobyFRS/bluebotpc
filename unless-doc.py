3cfbfd0eb4ea422300893591f930daf3c2dc5cad
GoobyFRS <68407017+GoobyFRS@users.noreply.github.com>
2021-03-18 20:56:42 -0600

    Update unless-doc.py

--- unless-doc.py
+++ unless-doc.py
@@ -1,25 +1,37 @@
 #Basic Python Part 1
-#This is a comment
+#This is a comment, most this file is notes tbh
 """
-This is just a file I created while skimming w3schools
-reading the python items. I have it saved as a .py file but if you run it
-it will just close out for no reason. Again, still learning all of this stuff.
--
-My goal is to create a script to check up on Ubuntu based system variables. Like uptime, hostname, cpu usage, and memory usage.
-No logging, just print the information. Simple, basic, good idea to learn on.
+This is just a multi-line comment. 
+The Python shell is called the REPL
+Read
+Evaluate
+Print
+Loop
+Indentation is important
+4 spaces instead of tab
 """
-
-#These are global variables. I have the type declared for two but you don't have too. Also they can be changed aftewrwards.
-x = int(128)
-y = int(256)
-z = "X is a smaller value"
-#Z will not overwrite z, so thats cool. Casing matters.
-Z = "Y is a larger value"
-
-print("Get the data type for global variable x")
-print(type(x))
-
-if x > y:
-	print(z)
-if y > x:
-	print("y is larger than x")
+#most everything below should be in REPL, so it doesn't actually do anything
+#import a library
+import math
+#module_name.attribute_name
+d = int(12)
+e = int(2)
+f = float(0.5)
+g = 10
+h = 5
+math.sqrt(d)
+"""
+int = integers - whole numbers only
+float = 64bit decimal numbers
+NoneType = placeholder / null
+bool = boolean logical values. True or False
+"""
+print("Hello Ruby, look at this!")
+# integers can be converted to binary, octal, and hexidecimal
+# 0b10 = 2
+#0o10 = 8
+#0x10 = 16
+# integers can be converted to binary, octal, and hexidecimal
+# 0b10 = 2
+#0o10 = 8
+#0x10 = 16
